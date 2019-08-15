9a57c4998a78c366df0241bcbe898bf931fdf136
ye.jingwei <yjw.j@foxmail.com>
2019-08-15 22:37:08 +0800

    乞丐版样式
    简单样式了解

--- miniprogram/pages/base/base.js
+++ miniprogram/pages/base/base.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    array:[1,2,3,4,5,6,7,8,9,10]
   },
 
   /**
@@ -62,5 +62,23 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /**
+   * 时间选择
+   */
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    // var i = 0;
+    // this.data.array[i] = i++;
+    this.setData({
+      index: e.detail.value
+    })
+  },
 })
